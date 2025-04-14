a7c308ec883ba0c18c42369e3ad0c7f6c00a9560
Jordan Baxter <jordan.baxter@ditto.live>
2025-04-14 12:24:36 -0700

    Removed README with todo in favor of issues.
    Also changes a key mapping.

--- stow/nvim/.config/README.md
+++
@@ -1,3 +0,0 @@
-# TODO:
-- [ ] Get nvim tree navigation working with hjkl
-- [ ] nvim-tree yank relative and absolute path of file under cursor.
--- stow/nvim/.config/nvim/lua/mappings.lua
+++ stow/nvim/.config/nvim/lua/mappings.lua
@@ -8,7 +8,6 @@ map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
 
 map("n", "<leader>gg", "<cmd>Neogit<cr>", { desc = "Git Neogit Open" })
-map("n", "<leader>gd", "<cmd>Neogit diff<cr>", { desc = "Git Neogit Diff" })
 map("n", "<leader>gc", "<cmd>Neogit commit<cr>", { desc = "Git Neogit Commit" })
 map("n", "<leader>gp", "<cmd>Neogit push<cr>", { desc = "Git Neogit Push" })
 map("n", "<leader>ti", function()
