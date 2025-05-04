073cd741f6d1e4207d9ce50cf05481bbbef7a5bb
baxterjo <j.bax080@gmail.com>
2025-05-04 11:14:13 -0700

    Updated test scripts

--- tests/test_linux.sh
+++ tests/test_linux.sh
@@ -2,10 +2,10 @@
 
 CONTAINER_NAME="test-linux-dotfile-install"
 
-docker run -td --name=$CONTAINER_NAME ubuntu:latest
+docker run --privileged -td --name=$CONTAINER_NAME ubuntu:latest
 
 docker cp ./dotfiles $CONTAINER_NAME:/
 
 docker exec -it $CONTAINER_NAME sh -c "apt-get update && apt-get -y install sudo curl"
 
-docker exec -e REPO_BRANCH=$(git rev-parse --abbrev-ref HEAD) -it $CONTAINER_NAME "/dotfiles"
+docker exec -e IN_CONTAINER=true REPO_BRANCH=$(git rev-parse --abbrev-ref HEAD) -it $CONTAINER_NAME "/dotfiles"
--- tests/test_linux_live.sh
+++ tests/test_linux_live.sh
@@ -4,11 +4,9 @@ CONTAINER_NAME="test-linux-dotfile-install-live"
 
 docker run -v $(pwd):$(pwd) -w $(pwd) -td --name=$CONTAINER_NAME ubuntu:latest
 
-docker cp ./dotfiles $CONTAINER_NAME:/
-
 docker exec -it $CONTAINER_NAME sh -c "apt-get update && apt-get -y install sudo curl"
 
-docker exec -it $CONTAINER_NAME sh -c "/dotfiles bootstrap"
+docker exec -it $CONTAINER_NAME bash -c "source ./dotfiles bootstrap"
 
-docker exec -it $CONTAINER_NAME "/bin/bash"
+docker exec -e IN_CONTAINER=true -it $CONTAINER_NAME "/bin/bash"
 
