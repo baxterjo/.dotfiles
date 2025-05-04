7573cc799e94f907446520bccb368a885cd95093
baxterjo <j.bax080@gmail.com>
2025-05-04 11:05:56 -0700

    Made oh-my-zsh install in unattended mode

--- scripts/oh-my-zsh.sh
+++ scripts/oh-my-zsh.sh
@@ -1,7 +1,7 @@
 install_oh_my_zsh() {
 	if [[ ! -f ~/.zshrc ]]; then
 		info "Installing oh my zsh..."
-		ZSH=~/.oh-my-zsh ZSH_DISABLE_COMPFIX=true sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+		ZSH=~/.oh-my-zsh ZSH_DISABLE_COMPFIX=true sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh) --unattended"
 
 		chmod 744 ~/.oh-my-zsh/oh-my-zsh.sh
 
