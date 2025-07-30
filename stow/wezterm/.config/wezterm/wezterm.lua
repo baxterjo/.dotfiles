b358776a2e11995e274539d3cff549fddcbbe8cc
Jordan Baxter <jordan.baxter@ditto.com>
2025-07-30 13:23:52 -0700

    Cleaned up wezterm config

--- stow/wezterm/.config/wezterm/wezterm.lua
+++ stow/wezterm/.config/wezterm/wezterm.lua
@@ -20,49 +20,9 @@ local pane_direction_map = {
   Up = 'Top',
 }
 
----Activate or create pane
----@param opts table
----@return table
-local function pane_activate_create(opts)
-  return {
-    key = opts.key,
-    mods = opts.mods,
-    action = wezterm.action_callback(function(_, pane)
-      local pane_at_direction = pane:tab():get_pane_direction(opts.direction)
-
-      if pane_at_direction then
-        pane_at_direction:activate()
-        return
-      end
-
-      pane:split({
-        direction = pane_direction_map[opts.direction],
-        domain = 'CurrentPaneDomain',
-      })
-    end),
-  }
-end
-
-local key_table_leader = { key = '/', mods = key_mod_panes }
+local catppuccin = wezterm.color.get_builtin_schemes()['Catppuccin Mocha']
 
 local keys = {
-  {
-    key = ',',
-    mods = 'CMD',
-    action = act.PromptInputLine({
-      description = 'Launch',
-      action = wezterm.action_callback(function(window, pane, line)
-        if line then
-          window:perform_action(
-            act.SpawnCommandInNewWindow({
-              args = wezterm.shell_split(line),
-            }),
-            pane
-          )
-        end
-      end),
-    }),
-  },
 
   {
     key = '?',
@@ -93,131 +53,6 @@ local keys = {
     action = act.ShowDebugOverlay,
   },
 
-  -- Panes
-  {
-    key = 'p',
-    mods = 'LEADER',
-    action = act.ActivateKeyTable({
-      name = 'pane',
-      timeout_milliseconds = 1500,
-    }),
-  },
-  {
-    key = 'd',
-    mods = key_mod_panes,
-    action = act.SplitHorizontal({ domain = 'CurrentPaneDomain' }),
-  },
-  {
-    key = 'D',
-    mods = 'SHIFT|' .. key_mod_panes,
-    action = act.SplitVertical({ domain = 'CurrentPaneDomain' }),
-  },
-
-  {
-    key = 'w',
-    mods = key_mod_panes,
-    action = act.CloseCurrentPane({ confirm = true }),
-  },
-
-  {
-    key = 'W',
-    mods = key_mod_panes .. '|SHIFT',
-    action = wezterm.action.CloseCurrentTab({ confirm = true }),
-  },
-
-  {
-    key = 'z',
-    mods = key_mod_panes,
-    action = act.TogglePaneZoomState,
-  },
-
-  {
-    key = '!',
-    mods = 'SHIFT|' .. key_mod_panes,
-    action = wezterm.action_callback(function(_win, pane)
-      pane:move_to_new_window()
-    end),
-  },
-
-  -- Activation
-  pane_activate_create({
-    key = 'h',
-    mods = key_mod_panes,
-    direction = 'Left',
-  }),
-
-  pane_activate_create({
-    key = 'l',
-    mods = key_mod_panes,
-    direction = 'Right',
-  }),
-
-  pane_activate_create({
-    key = 'k',
-    mods = key_mod_panes,
-    direction = 'Up',
-  }),
-
-  pane_activate_create({
-    key = 'j',
-    mods = key_mod_panes,
-    direction = 'Down',
-  }),
-
-  -- Size
-  {
-    key = 'H',
-    mods = 'SHIFT|' .. key_mod_panes,
-    action = act.AdjustPaneSize({ 'Left', 1 }),
-  },
-
-  {
-    key = 'J',
-    mods = 'SHIFT|' .. key_mod_panes,
-    action = act.AdjustPaneSize({ 'Down', 1 }),
-  },
-
-  {
-    key = 'K',
-    mods = 'SHIFT|' .. key_mod_panes,
-    action = act.AdjustPaneSize({ 'Up', 1 }),
-  },
-
-  {
-    key = 'L',
-    mods = 'SHIFT|' .. key_mod_panes,
-    action = act.AdjustPaneSize({ 'Right', 1 }),
-  },
-
-  -- Rotate
-  {
-    key = 'r',
-    mods = key_mod_panes,
-    action = act.RotatePanes('CounterClockwise'),
-  },
-
-  {
-    key = 'R',
-    mods = 'SHIFT|CMD',
-    action = act.RotatePanes('Clockwise'),
-  },
-
-  -- Select and focus
-  {
-    key = 's',
-    mods = key_mod_panes,
-    action = act.PaneSelect,
-  },
-
-  -- Select and swap
-  {
-    key = 'S',
-    mods = 'SHIFT|' .. key_mod_panes,
-    action = act.PaneSelect({
-      mode = 'SwapWithActive',
-    }),
-  },
-
   -- Tabs
   {
     key = 't',
@@ -229,15 +64,6 @@ local keys = {
   -- Activate Tabs
   { key = 'H', mods = 'SHIFT|' .. key_mod_panes, action = act.ActivateTabRelative(-1) },
   { key = 'L', mods = 'SHIFT|' .. key_mod_panes, action = act.ActivateTabRelative(1) },
-  {
-    key = 'o',
-    mods = key_mod_panes,
-    action = act.ActivateLastTab,
-  },
-
-  -- Swap Tabs
-  { key = 'H', mods = 'SHIFT|CTRL|' .. key_mod_panes, action = act.MoveTabRelative(-1) },
-  { key = 'L', mods = 'SHIFT|CTRL|' .. key_mod_panes, action = act.MoveTabRelative(1) },
 
   -- Utils
 
@@ -245,103 +71,6 @@ local keys = {
     key = '0',
     mods = key_mod_panes,
     action = act.ResetFontSize,
-  },
-
-  {
-    key = 'v',
-    mods = 'SHIFT|' .. key_mod_panes,
-    action = act.ActivateCopyMode,
-  },
-
-  {
-    key = 'L',
-    mods = 'CTRL|SHIFT',
-    action = act.QuickSelectArgs({
-      patterns = {
-        'https?://\\S+',
-      },
-    }),
-  },
-  {
-    key = 'L',
-    mods = 'CMD|SHIFT',
-    action = act.QuickSelectArgs({
-      patterns = {
-        'https?://\\S+',
-      },
-      action = wezterm.action_callback(function(window, pane)
-        local url = window:get_selection_text_for_pane(pane)
-        wezterm.open_with(url)
-      end),
-    }),
-  },
-
-  -- Jump word to the left
-  {
-    key = 'LeftArrow',
-    mods = 'OPT',
-    action = act.SendKey({
-      key = 'b',
-      mods = 'ALT',
-    }),
-  },
-
-  -- Jump word to the right
-  {
-    key = 'RightArrow',
-    mods = 'OPT',
-    action = act.SendKey({ key = 'f', mods = 'ALT' }),
-  },
-
-  -- Go to beginning of line
-  {
-    key = 'LeftArrow',
-    mods = 'CMD',
-    action = act.SendKey({
-      key = 'a',
-      mods = 'CTRL',
-    }),
-  },
-
-  -- Go to end of line
-  {
-    key = 'RightArrow',
-    mods = 'CMD',
-    action = act.SendKey({ key = 'e', mods = 'CTRL' }),
-  },
-
-  -- Bypass
-  { key = '/', mods = 'CTRL', action = act.SendKey({ key = '/', mods = 'CTRL' }) },
-  { key = 'q', mods = 'CTRL', action = act.SendKey({ key = 'q', mods = 'CTRL' }) },
-  { key = 'k', mods = 'CTRL', action = act.SendKey({ key = 'k', mods = 'CTRL' }) },
-  { key = 'i', mods = 'CTRL', action = act.SendKey({ key = 'i', mods = 'CTRL' }) },
-}
-
-local key_tables = {
-  pane = {
-    {
-      key = 't',
-      action = wezterm.action_callback(function(_win, pane)
-        pane:move_to_new_tab()
-      end),
-    },
-
-    -- vertical slim panel
-    {
-      key = 'v',
-      action = act.SplitPane({
-        direction = 'Right',
-        size = { Percent = 35 },
-      }),
-    },
-
-    {
-      key = 'h',
-      action = act.SplitPane({
-        direction = 'Down',
-        size = { Percent = 35 },
-      }),
-    },
   },
 }
 
@@ -377,118 +106,23 @@ local process_icons = {
   ['ruby'] = wezterm.nerdfonts.cod_ruby,
 }
 
-local function get_current_working_dir(tab)
-  local url = tab.active_pane and tab.active_pane.current_working_dir or { file_path = '' }
-  local path = url.file_path
-  if path:sub(-1) == '/' then
-    path = path:sub(1, #path - 1)
-  end
-
-  local HOME_DIR = os.getenv('HOME')
-
-  return path == HOME_DIR and '~' or string.gsub(path, '(.*[/\\])(.*)', '%2')
-end
-
-local function get_process(tab)
-  if not tab.active_pane or tab.active_pane.foreground_process_name == '' then
-    return nil
-  end
-
-  local process_name = string.gsub(tab.active_pane.foreground_process_name, '(.*[/\\])(.*)', '%2')
-  if string.find(process_name, 'kubectl') then
-    process_name = 'kubectl'
-  end
-
-  return process_icons[process_name] or string.format('[%s]', process_name)
-end
-
-wezterm.on('format-tab-title', function(tab, tabs, panes, config, hover, max_width)
-  local has_unseen_output = false
-  if not tab.is_active then
-    for _, pane in ipairs(tab.panes) do
-      if pane.has_unseen_output then
-        has_unseen_output = true
-        break
-      end
-    end
-  end
-
-  local cwd = wezterm.format({
-    { Text = get_current_working_dir(tab) },
-  })
-
-  local process = get_process(tab)
-  local title = process and string.format(' %s (%s) ', process, cwd) or ' [?] '
-
-  if has_unseen_output then
-    return {
-      { Foreground = { Color = '#28719c' } },
-      { Text = title },
-    }
-  end
-
-  return {
-    { Text = title },
-  }
-end)
-
-local background = '#292D3E'
-wezterm.on('update-right-status', function(window, pane)
-  local process = ''
-
-  if state.debug_mode then
-    local info = pane:get_foreground_process_info()
-    if info then
-      process = info.name
-      for i = 2, #info.argv do
-        process = info.argv[i]
-      end
-    end
-  end
-
-  local status = (#process > 0 and ' | ' or '')
-  local name = window:active_key_table()
-  if name then
-    status = string.format('󰌌  { %s }', name)
-  end
-
-  if window:get_dimensions().is_full_screen then
-    status = status .. wezterm.strftime(' %R ')
-  end
-
-  window:set_right_status(wezterm.format({
-    { Foreground = { Color = '#7eb282' } },
-    { Text = process },
-    { Foreground = { Color = '#808080' } },
-    { Text = status },
-  }))
-end)
-
+-- Colors defined here will override the Catppuccin scheme
+-- For some reason the catppuccin tab colors do not propogate in the theme
 local colors = {
-  background = background,
-  cursor_bg = '#FFCB6B',
-  cursor_fg = background,
-  cursor_border = '#FFCB6B',
-  selection_fg = background,
-  selection_bg = '#FFCB6B',
-  quick_select_label_bg = { Color = '#60b5de' },
-  quick_select_label_fg = { Color = '#ffffff' },
-  quick_select_match_bg = { Color = '#c07d9e' },
-  quick_select_match_fg = { Color = '#ffffff' },
   tab_bar = {
-    background = background,
-    inactive_tab_edge = 'rgba(28, 28, 28, 0.9)',
+    background = catppuccin.tab_bar.background,
+    inactive_tab_edge = catppuccin.tab_bar.inactive_tab_edge,
     active_tab = {
-      bg_color = background,
-      fg_color = '#FFFFFF',
+      bg_color = catppuccin.tab_bar.active_tab.bg_color,
+      fg_color = catppuccin.tab_bar.active_tab.fg_color,
     },
     inactive_tab = {
-      bg_color = background,
-      fg_color = '#808080',
+      bg_color = catppuccin.tab_bar.inactive_tab.bg_color,
+      fg_color = catppuccin.tab_bar.inactive_tab.fg_color,
     },
     inactive_tab_hover = {
-      bg_color = background,
-      fg_color = '#808080',
+      bg_color = catppuccin.tab_bar.inactive_tab_hover.bg_color,
+      fg_color = catppuccin.tab_bar.inactive_tab_hover.fg_color,
     },
   },
 }
@@ -496,23 +130,10 @@ local colors = {
 local config = {
   adjust_window_size_when_changing_font_size = false,
   audible_bell = 'Disabled',
-  background = {
-    -- {
-    --   source = {
-    --     File = '/Users/bmayo/.dotfiles/background/goya.jpg',
-    --   },
-    --   width = 'Cover',
-    --   height = 'Cover',
-    --   hsb = { brightness = 0.3 },
-    --   vertical_align = 'Middle',
-    --   horizontal_align = 'Center',
-    -- },
-  },
   canonicalize_pasted_newlines = 'None',
-  color_scheme = 'Classic Dark (base16)',
+  color_scheme = 'Catppuccin Mocha',
   colors = colors,
   command_palette_font_size = 16.0,
-  command_palette_bg_color = '#1c1c1c',
   cursor_blink_rate = 500,
   default_cursor_style = 'BlinkingUnderline',
   default_cwd = wezterm.home_dir,
@@ -527,15 +148,9 @@ local config = {
   font_size = 14.0,
   -- Disable font ligatures
   harfbuzz_features = { 'calt=1', 'clig=0', 'liga=0', 'zero', 'ss01' },
-  hide_tab_bar_if_only_one_tab = false,
+  hide_tab_bar_if_only_one_tab = true,
   hyperlink_rules = wezterm.default_hyperlink_rules(),
-  inactive_pane_hsb = {
-    saturation = 1.0,
-    brightness = 0.6,
-  },
   keys = keys,
-  key_tables = key_tables,
-  leader = key_table_leader,
   max_fps = 120,
   mouse_bindings = {
     {
@@ -550,11 +165,6 @@ local config = {
       action = act.OpenLinkAtMouseCursor,
     },
   },
-  quick_select_patterns = {
-    '[0-9a-f]{7,40}', -- hashes
-    '[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}', -- uuids
-    'https?:\\/\\/\\S+',
-  },
   quote_dropped_files = 'Posix',
   scrollback_lines = 10000,
   send_composed_key_when_left_alt_is_pressed = false,
@@ -568,15 +178,12 @@ local config = {
   tab_max_width = 80,
   underline_position = -4,
   use_fancy_tab_bar = true,
-  -- window_background_opacity = 1,
-  -- window_background_opacity = 0.6,
-  -- macos_window_background_blur = 10,
   window_decorations = 'RESIZE',
   window_frame = {
     font = wezterm.font({ family = font }),
     font_size = 18.0,
-    active_titlebar_bg = colors.background,
-    inactive_titlebar_bg = colors.background,
+    active_titlebar_bg = catppuccin.background,
+    inactive_titlebar_bg = catppuccin.background,
   },
 }
 
